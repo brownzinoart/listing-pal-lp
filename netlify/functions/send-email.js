985f3ce3c6c74f5adbcae9d3477006e63741baa1
brownzinoart <brownzinoart@gmail.com>
2025-07-02 20:19:45 -0400

    restore: all-black logo version that worked after 5:30 PM
    - Restore logo.png, welcome-email.html, and send-email.js from commit 89a5e2c
    - This was the all-black logo version that displayed correctly in emails

--- netlify/functions/send-email.js
+++ netlify/functions/send-email.js
@@ -118,7 +118,7 @@ const emailTemplate = `<!DOCTYPE html>
                             <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
                                 <tr>
                                     <td align="center">
-                                                                <img src="https://listingpal.netlify.app/logo_email.png"
+                                                                <img src="https://listingpal.netlify.app/logo.png"
                         alt="ListingPal Logo"
                         width="200"
                         height="auto"
@@ -328,7 +328,7 @@ const emailTemplate = `<!DOCTYPE html>
                             <table role="presentation" cellspacing="0" cellpadding="0" border="0" width="100%">
                                 <tr>
                                     <td align="center" style="padding-bottom: 20px;">
-                                                                                 <img src="https://listingpal.netlify.app/logo_email.png"
+                                                                                 <img src="https://listingpal.netlify.app/logo.png"
                                          alt="ListingPal Footer Logo"
                                          width="150"
                                          height="auto"
